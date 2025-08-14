c3f4abc1e7ba57211c0937bd2169220b45b083a6
tush30 <tushrathod2002@gmailcom>
2025-08-15 01:00:25 +0530

    new commit

--- src/test/java/com/logintest/e2e/pages/HomePage.java
+++ src/test/java/com/logintest/e2e/pages/HomePage.java
@@ -14,7 +14,7 @@ public class HomePage {
     }
 
     public boolean isLoaded() {
-        System.out.println("hello tushar Rathod");
+        System.out.println("hello tushar Rathod kavya");
         return !driver.findElements(welcomeBanner).isEmpty();
     }
 }
